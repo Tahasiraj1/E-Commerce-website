ab84ee2378bbc5ea57b7c70aabd88f0f0807ebcf
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-02 01:30:22 +0500

    Fetch and display scents from Sanity; replace hardcoded list with dynamic data

--- components/Scents.tsx
+++ components/Scents.tsx
@@ -1,69 +1,61 @@
 "use client"
 
+import { useState, useEffect } from "react";
 import { Card, CardContent, CardFooter } from "./ui/card";
 import Image from "next/image";
+import { urlFor } from "@/sanity/lib/image";
+import { client } from '@/sanity/lib/client';
+import { Image as SanityImage } from '@sanity/types';
+
+interface Product {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  image: SanityImage[];
+  ratings: string;
+  tags: string[];
+  description: string;
+}  
 
 export default function Scents() {
+  const [scents, setScents] = useState<Product[]>([]);
 
-  const list = [
-    {
-      title: "Orange",
-      img: "/images/black-1984277.jpg",
-      price: "$5.50",
-    },
-    {
-      title: "Tangerine",
-      img: "/images/black-1984277.jpg",
-      price: "$3.00",
-    },
-    {
-      title: "Raspberry",
-      img: "/images/black-1984277.jpg",
-      price: "$10.00",
-    },
-    {
-      title: "Lemon",
-      img: "/images/black-1984277.jpg",
-      price: "$5.30",
-    },
-    {
-      title: "Avocado",
-      img: "/images/black-1984277.jpg",
-      price: "$15.70",
-    },
-    {
-      title: "Lemon 2",
-      img: "/images/black-1984277.jpg",
-      price: "$8.00",
-    },
-    {
-      title: "Banana",
-      img: "/images/black-1984277.jpg",
-      price: "$7.50",
-    },
-    {
-      title: "Watermelon",
-      img: "/images/black-1984277.jpg",
-      price: "$12.20",
-    },
-  ];
+  useEffect(() => {
+      const fetchProducts = async () => {
+          const query = `*[_type == "scents"]{
+          id,
+          name,
+          quantity,
+          price,
+          image,
+          ratings,
+          tags,
+          description,
+          }`;
+
+          const products = await client.fetch(query);
+          setScents(products);
+      };
+      fetchProducts();
+  }, []);
 
   return (
-    <div className="gap-2 grid grid-cols-2 md:grid-cols-4 px-10">
-      {list.map((item, index) => (
-        <Card key={index}>
+    <div className="bg-[#0a1a32ff] gap-2 grid grid-cols-2 md:grid-cols-4 px-4 md:px-10">
+      {scents.map((scent, index) => (
+        <Card key={index} className="border-[#0a1a32ff] bg-transparent/40 text-white">
           <CardContent className="overflow-visible p-0">
             <Image
-              alt={item.title}
-              className="w-full rounded-xl object-cover  "
-              src={item.img}
+              alt={scent.name}
+              className="w-full rounded-t-xl object-cover  "
+              src={urlFor(scent.image[0]).url()}
               width={200}
               height={200}
             />
           </CardContent>
-          <CardFooter className="text-small justify-between bg-transparent">
-            <b>{item.title}</b>
-            <p className="text-default-500">{item.price}</p>
+          <CardFooter className="text-small mt-4 justify-between rounded-b-xl">
+            <b>{scent.name}</b>
+            <p className="text-default-500">{scent.price}</p>
           </CardFooter>
         </Card>
       ))}
