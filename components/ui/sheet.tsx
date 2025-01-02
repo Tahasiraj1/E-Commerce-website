ef298f4dd2b96085dba7b4352b81c3b164d7c821
Tahasiraj1 <tahasiraj242@gmail.com>
2025-01-02 06:47:21 +0500

    Remove unused icon imports in Sheet component to streamline code

--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -3,7 +3,7 @@
 import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
-import { ArrowBigRight, ArrowBigRightDash, ArrowDownRight, ArrowRight, X,  } from "lucide-react"
+import { ArrowRight  } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
